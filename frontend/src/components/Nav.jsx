925abcd0fa372e5ee9ee41fefab3c569631f0569
Nico <nicholasloperena@gmail.com>
2025-03-03 17:18:34 -0500

    removed squeaky clean button

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -112,7 +112,7 @@ export default function Nav() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
-          <SqueakyCleanButton />
+         
         </div>
 
         {/* Mobile Navigation */}
